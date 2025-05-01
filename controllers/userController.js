6cb5e2726dd8c7298e7eebec6cf559810273f139
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-05-01 18:04:12 +0100

    Checking for errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -103,12 +103,25 @@ export const staffCreateUser = async (req, res) => {
   }
 };
 
-
 // admin only
 export const getUsers = async (req, res) => {
   try {
     const users = await UserSchema.find().select("-password");
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+    const {userId} = req.query;
+  try {
+    const targetUser = await UserSchema.findById(userId).select("-password");
+
+    if (!targetUser) return res.status(404).json({ message: "User not found" });
+
+    return res.status(200).json(targetUser);
+    
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
