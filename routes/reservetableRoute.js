9c9d3bf5c7da2173117cc8f77fee28e8633c8541
OLAYEMI <awoyemiolayemi27@gmail.com>
2025-05-06 20:21:36 +0100

    made changes

--- routes/reservetableRoute.js
+++ routes/reservetableRoute.js
@@ -3,7 +3,7 @@ const reserveRouter = express.Router()
 import { createReserveTable, deleteReserveTable, updateReserveTable, getAllReserveTable } from "../controllers/ReserveTable.js";
 import {protect, authorizeRoles} from '../middlewares/authMiddleware.js'
 
-reserveRouter.post('/create-reserve-table/:tableId', createReserveTable)
+reserveRouter.post('/create-reserve-table/:tableId', protect, createReserveTable)
 reserveRouter.get('/getAll-reserve-table', protect, authorizeRoles('admin'), getAllReserveTable)
 reserveRouter.put('/update-reserve-table/:id', protect, updateReserveTable) 
 reserveRouter.delete('/delete-reserve-table/:id', protect, authorizeRoles('admin'), deleteReserveTable)
