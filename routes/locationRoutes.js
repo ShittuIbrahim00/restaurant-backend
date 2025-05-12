fe32236bb2ccddc3903f132d5ebfc19bc3deb836
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-05-12 14:38:07 +0100

    Empty

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -9,6 +9,6 @@ LocationRouter.get("/restaurant-locations", getLocationsByRestaurant);
 LocationRouter.get("/restaurant-location", getLocationById);
 LocationRouter.put("/restaurant-location/:id", protect, authorizeRoles("admin", "restaurant-owner"), updateLocation);
 
-LocationRouter.delete("/restaurant-location?:id", protect, authorizeRoles("admin", "restaurant-owner"), deleteLocation);
+LocationRouter.delete("/restaurant-location", protect, authorizeRoles("admin", "restaurant-owner"), deleteLocation);
 
 export default LocationRouter;
