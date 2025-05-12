fd31008154c8fb26f384b6c190073c16a7ab0c6e
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-05-12 11:03:05 +0100

    Empty

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createRestaurant, deleteRestaurant, getAllRestaurants, getRestaurantById, updateRestaurant } from "../controllers/restaurantController.js";
+import { authorizeRoles, protect } from "../middlewares/authMiddleware.js";
 
 const restaurantRouter = express.Router();
 
 restaurantRouter.post("/create-restaurant", createRestaurant);
 restaurantRouter.get("/restaurants", getAllRestaurants);
 restaurantRouter.get("/restaurant", getRestaurantById);
-restaurantRouter.put("/restaurant", updateRestaurant);
+restaurantRouter.put("/restaurant", protect, authorizeRoles("admin"), updateRestaurant);
 restaurantRouter.delete("/restaurant", deleteRestaurant);
 
 export default restaurantRouter;
