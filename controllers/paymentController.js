805ed39cbd967c970063ccb834d074875cc03453
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-05-14 11:48:35 +0100

    Testing again

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,3 +1,6 @@
+import createTableSchema from "../models/CreateTable";
+import UserSchema from "../models/userModel";
+
 export const createTableReservationWithOptionalPayment = async (req, res) => {
     try {
       const { userId, tableId, reservation_Date, reservation_Time, qty_persons } = req.body;
@@ -6,7 +9,7 @@ export const createTableReservationWithOptionalPayment = async (req, res) => {
         return res.status(400).json({ success: false, msg: "All fields are required" });
       }
   
-      const user = await User.findById(userId);
+      const user = await UserSchema.findById(userId);
       const table = await createTableSchema.findById(tableId);
       if (!user || !table) {
         return res.status(404).json({ success: false, msg: "User or Table not found" });
